548f9c11beeb72e41078d09f30f075ae3e07e991
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-01 23:30:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyAppendKitten(name){
   return kittens
 }
 
-function destructivelyRemoveLastKitten(name){
+function destructivelyPrependKitten(name){
   kittens.unshift(name)
   return kittens
 }
